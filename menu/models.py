896b6f1a59d5bb07504d0ead89dd4a2870822484
Francois Nsengimana <francknsengimana@gmail.com>
2026-06-23 11:53:10 +0100

    Add Pizza model and initial migration with fields for name, ingredients, price, and vegetarian status

--- menu/models.py
+++ menu/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Pizza(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-
-    def __str__(self):
-        return self.name
+    nom = models.CharField(max_length=200)
+    ingredients = models.CharField(max_length=400)
+    prix = models.FloatField(default=0)
+    vegetarienne = models.BooleanField(default=False)
