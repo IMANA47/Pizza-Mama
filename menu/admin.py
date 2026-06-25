de3f07c0b99919393837f79cf231c21ef6fea8e9
Francois Nsengimana <francknsengimana@gmail.com>
2026-06-25 10:30:38 +0100

    Add LocaleMiddleware to settings and register Pizza model in admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Pizza
 # Register your models here.
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'ingredients', 'prix', 'vegetarienne')
+    search_fields = ('nom', 'ingredients')
+
+admin.site.register(Pizza, PizzaAdmin)
